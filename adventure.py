3101237afb87e44ef90d86c656ffcd2f0eca4328
ProgrammerInProgress <156963228+ProgrammerInProgress@users.noreply.github.com>
2024-02-06 09:14:53 -0500

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -23,8 +23,9 @@ if __name__ == "__main__":
     with open('map.txt') as map_file, open('locations.txt') as location_file, open('items.txt') as item_file:
         w = World(map_file, location_file, item_file)
     p = Player(2, 1)  # set starting location of player; you may change the x, y coordinates here as appropriate
-    safe = w.get_location(1, 1)
-    safe = PuzzleLocation(safe.position, safe.brief_description, safe.long_description, safe.available_directions, safe.items, 416)
+    safe = w.get_location(1, 1)  # set puzzle variable
+    safe_long = safe.long_description
+    safe = PuzzleLocation(safe.position, safe_long, 416)
     safe_crack_attempts = 0
     quit_game = False
     cracked_safe = False
@@ -34,6 +35,8 @@ if __name__ == "__main__":
     # objectives represents which items have been deposited at the correct locations.
     # The player can only win if all values in this variable are True
     objectives = {'T_Card': False, 'Lucky_Pen': False, 'Backpack': False, 'Cheat_Sheet': False}
+    deposit = {'T_Card': 'Robarts entrance', 'Lucky_Pen': 'study room', 'Backpack': 'Starbucks',
+               'Cheat_Sheet': 'tree by Innis'}
 
     s = ('You\'ve got an important exam coming up this evening, and you\'ve been studying for weeks. Last night was a\n'
          'particularly late night on campus. You had difficulty focusing, so rather than staying in one place, you\n'
@@ -48,6 +51,13 @@ if __name__ == "__main__":
          'starts tonight?\n\n')
     print('\n\n' + s)
 
+    """ The game loop only stops when the player runs out of moves or when the player deposited all items in the
+    correct locations AND solved the puzzle. What this loop basically does is ask the player to type an action and then
+    performs the action. So if the player wants to go East, the player's x value is incremented by one. If the player
+    wants to pick up an item, player.pick_up_item() is called. Basically the program asks the player for an action,
+    interprets it, and then calls other functions to make it happen through conditionals. Think of our adventure.py
+    as the CPU of our game; it controls everything and makes sure the game is running as expected.
+    """
     while not p.victory and not quit_game and num_moves > 0:
         location = w.get_location(p.x, p.y)
 
@@ -57,6 +67,7 @@ if __name__ == "__main__":
         else:
             print(location.long_description)
 
+        # If the player is in LOCATION 2 (where the safe is found), a new action is shown to the player - 'open safe'
         if 'safe' in location.long_description:
             menu = ["look", "inventory", "score", "quit", "back", 'open safe']
         else:
@@ -81,10 +92,12 @@ if __name__ == "__main__":
         choice = choice.title()
 
         if choice == 'Look':
+            # since what description is printed (brief/long) depends on location.visited, we
+            # simply set it to false for it to print the full description
             location.visited = False
         elif choice == 'Inventory':
             choice = input('\nTo view items in your inventory, enter view.\nTo drop items, enter drop\nYour choice: ')
-            inventory_items = [item.name for item in p.inventory]
+            inventory_items = [element.name for element in p.inventory]
 
             if len(p.inventory) == 0:
                 print('No items in your inventory!!!')
@@ -93,17 +106,18 @@ if __name__ == "__main__":
             elif choice == 'drop':
                 choice = input(f'Your inventory has {inventory_items}\nWhich item do you want to drop: ')
                 if p.drop_items(location, choice, objectives):
-                    last_move = 'dropped ' + choice
+                    last_move = 'Dropped ' + choice
             else:
                 print('Not a valid choice!')
 
         elif choice == 'Score':
             print('Your current score is ', p.score)
-        elif choice == 'Quit':
+        elif choice == 'Quit':  # Only way for the player to stop the game without brute forcing the run to stop
             quit_game = True
         elif choice == 'Open Safe' and 'open safe' in menu:
             if not cracked_safe:
                 if safe.solve_puzzle(safe_crack_attempts):
+                    # Player successfully solved the puzzle, not able to replay the puzzle
                     cracked_safe = True
                     safe_crack_attempts += 1
                     p.score += 15
@@ -111,14 +125,17 @@ if __name__ == "__main__":
             else:
                 print('You already solved the puzzle and opened the safe!!!')
         elif choice == 'Objectives':
-            deposit = {'T_Card': 'Robarts entrance', 'Lucky_Pen': 'study room', 'Backpack': 'Starbucks', 'Cheat_Sheet': 'tree by Innis'}
+            # Prints the deposit locations of all items not already in the correct deposit location
+            # This way, the player is reminded of the game's goals and is not wasting their num_moves count
             for objective in objectives:
                 if not objectives[objective]:
                     print(f'The {objective} needs to be deposited at the {deposit[objective]}')
             print('You need to find a code to open the safe!!!')
         elif choice == 'Pick Up Items':
             if location.available_actions() and location.available_actions()[0] == 'Pick up items':
-                items_to_pick_up = [item for item in location.items if item not in p.inventory]
+                # If player is able to pick up the items, all are picked up
+                # If you drop an item and pick up the items in the location, you also pick up the dropped item!!!
+                items_to_pick_up = [i for i in location.items if i not in p.inventory]
 
                 for item in items_to_pick_up:
                     p.pick_up_item(location, item)
@@ -133,17 +150,20 @@ if __name__ == "__main__":
         elif choice == 'Back':
             num_moves += 1
             item = ''
+            # this variable stores a str description of the last action taken by the player
+            # to decipher it, the program looks at the last word in the variable (it is always an action) and negates it
             if last_move != '':
                 item = last_move.split()
                 item = item[len(item) - 1]
 
-            if 'picked up' in last_move:
+            if 'picked up' in last_move:  # If the player picked up an item, drop it
                 p.drop_items(location, item, objectives)
-            elif 'dropped' in last_move:
+            elif 'dropped' in last_move:  # If player dropped an item, pick it up
                 p.pick_up_item(location, location.items[len(location.items) - 1])
-            elif 'Go' in last_move:
-                opposite_direction = {'Go East': 'Go West', 'Go West': 'Go East', 'Go North': 'Go South', 'Go South': 'Go North'}
-                p.update(opposite_direction[last_move])
+            elif 'Go' in last_move:  # Player goes back to last location
+                opposite_direction = {'East': 'West', 'West': 'East', 'North': 'South',
+                                      'South': 'North'}
+                p.update('Go ' + opposite_direction[last_move])
             else:
                 print('There is no previous move recorded to go back to!')
         elif choice in ['Go East', 'Go North', 'Go South', 'Go West']:  # Direction not allowed, prints LOCATION -1
@@ -154,16 +174,24 @@ if __name__ == "__main__":
             if choice in ['East', 'West', 'South', 'North']:
                 print('To move around, enter direction in the format: Go [direction]')
 
-        if all(objectives[objective] is True for objective in objectives):
+        if all(objectives[mission] is True for mission in objectives):
             p.victory = True
 
         print('')
 
-    if p.victory and cracked_safe:
+    if p.victory and cracked_safe:  # Player is victorious
         print('Congratulations! You found all items and cracked the safe!\nYou got to the Exam Room on time.')
         print('You won the game!!!')
-    elif p.victory:
+    elif p.victory:  # Semi win, did not solve puzzle!
         print('You found all items but could not open the safe\\nGAME OVER')
     else:  # Means the player wanted to quit the game or no moves left
         print('Ran out of moves!\nGAME OVER')
-        
+
+if __name__ == '__main__':
+    # Uncomment to test
+    # import doctest
+    # doctest.testmod(verbose=True)
+    # import python_ta
+    # python_ta.check_all(config={
+    #     'max-line-length': 120
+    # })
